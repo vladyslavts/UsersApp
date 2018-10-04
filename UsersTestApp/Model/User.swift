db06c63d1cf4df471dfbd0383345543217ac0d59
Vladyslav <>
2018-10-04 11:05:22 +0300

    Added User model

---
+++ UsersTestApp/Model/User.swift
@@ -0,0 +1,34 @@
+//
+//  User.swift
+//  UsersTestApp
+//
+//  Created by Andrii Mazepa on 10/4/18.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    
+    let firstName: String
+    let lastName: String
+    let email: String
+    let phoneNumber: String
+    let photoURL: String
+    
+    init(withResponse response: Result) {
+        self.firstName = response.name.first
+        self.lastName = response.name.last
+        self.email = response.email
+        self.phoneNumber = response.phone
+        self.photoURL = response.picture.medium
+    }
+    
+    override init() {
+        self.firstName = ""
+        self.lastName = ""
+        self.email = ""
+        self.phoneNumber = ""
+        self.photoURL = ""
+    }
+}
