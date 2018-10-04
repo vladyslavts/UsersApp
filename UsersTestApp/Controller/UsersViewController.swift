cf7b5cbca4834e023db4a2065a60714bf877a46c
Vladyslav <>
2018-10-04 10:54:43 +0300

    Added Users tableview, MVC structure

---
+++ UsersTestApp/Controller/UsersViewController.swift
@@ -0,0 +1,46 @@
+//
+//  UsersViewController.swift
+//  UsersTestApp
+//
+//  Created by Andrii Mazepa on 10/4/18.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension UsersViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    
+}
+
+extension UsersViewController: UITableViewDelegate {
+    
+}
