a045b5b45294535980494e079fe4c3b231476d59
Vladyslav <>
2018-10-05 12:36:08 +0300

    Connected Edit info cell

--- UsersTestApp/Controller/EditUserInfoViewController.swift
+++ UsersTestApp/Controller/EditUserInfoViewController.swift
@@ -13,6 +13,11 @@ class EditUserInfoViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var userPic: UIImageView!
     
+    override func loadView() {
+        super.loadView()
+        tableView.register(UINib(nibName: EditInfoTableViewCell.identifier(), bundle: nil), forCellReuseIdentifier: EditInfoTableViewCell.identifier())
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -29,7 +34,7 @@ extension EditUserInfoViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: EditInfoTableViewCell.identifier(), for: indexPath) as! EditInfoTableViewCell
         return cell
     }
     
